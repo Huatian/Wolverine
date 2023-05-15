70970f689b8f19ab7911029c7b5bb056350d9e30
WangLingwei <773512457@qq.com>
2023-05-15 09:22:26 +0800

    modify act ext

--- wolverine/src/main/java/com/github/huatian/common/util/AppCompatActivityExt.kt
+++ wolverine/src/main/java/com/github/huatian/common/util/AppCompatActivityExt.kt
@@ -4,10 +4,22 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 fun AppCompatActivity.hideSoftKeyBoard(view: View) {
 
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(view.windowToken, 0)
 
+}
+
+fun AppCompatActivity.checkSelfPermissionCompat(permission: String) =
+    ActivityCompat.checkSelfPermission(this, permission)
+
+fun AppCompatActivity.shouldShowRequestPermissionRationaleCompat(permission: String) =
+    ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
+
+fun AppCompatActivity.requestPermissionsCompat(permissionsArray: Array<String>,
+                                               requestCode: Int) {
+    ActivityCompat.requestPermissions(this, permissionsArray, requestCode)
 }
